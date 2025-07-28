9a258588e54be09a701deab03600e9a7c775becd
Teo <75574673+sunsakis@users.noreply.github.com>
2025-07-28 22:51:38 +0300

    logo sensitive to color theme now

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,8 @@
 import { WalletMultiButton } from '@demox-labs/miden-wallet-adapter-reactui';
+import { useTheme } from "@/components/theme-provider";
 
 export function Header() {
+  const { theme } = useTheme();
 
   return (
     <div className="flex items-center justify-between p-3 sm:p-4">
@@ -9,12 +11,12 @@ export function Header() {
         <img 
           src="/blackhatmini.png" 
           alt="Zoro Hat" 
-          className="h-12 w-auto mirror-x"
+          className={`h-12 w-auto mirror-x ${theme === 'light' ? 'invert' : ''}`}
         />
         <img 
           src="/logotype.png" 
           alt="Zoro Logotype" 
-          className="h-4 w-auto"
+          className={`h-4 w-auto ${theme === 'light' ? 'invert' : ''}`}
         />
       </div>
 
@@ -22,7 +24,6 @@ export function Header() {
       <div className="flex items-center gap-2">
           <WalletMultiButton className="z-10 !py-3 sm:!py-4 !rounded-xl !font-medium !text-sm sm:!text-lg !bg-transparent !border-none !text-muted-foreground hover:!text-foreground hover:!bg-gray-500/10" />
       </div>
-
     </div>
   );
 }
