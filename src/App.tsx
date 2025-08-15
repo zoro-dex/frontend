0d730a4e5fb157ef34879beb2b963a0729d07156
0x0-mico <mic0@mic0.dev>
2025-08-15 20:55:38 +0200

    Landing Page as only page

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { NablaAntennaProvider } from './components/PriceFetcher';
-import SwapPage from './pages/Swap';
-import FaucetPage from './pages/Faucet';
+// import SwapPage from './pages/Swap';
+// import FaucetPage from './pages/Faucet';
 import LandingPage from './pages/Landing';
 
 function AppRouter() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<SwapPage />} />
-        <Route path="/faucet" element={<FaucetPage />} />
-        <Route path="/landing" element={<LandingPage />} />
+        <Route path="/" element={/*<SwapPage />*/<LandingPage />} />
+        {/*<Route path="/faucet" element={<FaucetPage />} />
+        <Route path="/landing" element={<LandingPage />} />*/}
       </Routes>
     </Router>
   );
