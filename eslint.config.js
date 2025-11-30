d8deba334d521c34187c1024513086e3d9c0f499
mico <mic0@mic0.dev>
2025-11-30 04:57:08 +0100

    Removed commented-out eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -41,26 +41,3 @@ export default defineConfig([
     extends: ['css/recommended'],
   },
 ]);
-
-// import js from '@eslint/js';
-// import reactHooks from 'eslint-plugin-react-hooks';
-// import { globalIgnores } from 'eslint/config';
-// import globals from 'globals';
-// import tseslint from 'typescript-eslint';
-
-// export default tseslint.config([
-//   globalIgnores(['dist']),
-//   {
-//     files: ['**/*.{ts,tsx}'],
-//     extends: [
-//       js.configs.recommended,
-//       tseslint.configs.recommended,
-//       reactHooks.configs['recommended-latest'],
-//       reactRefresh.configs.vite,
-//     ],
-//     languageOptions: {
-//       ecmaVersion: 2020,
-//       globals: globals.browser,
-//     },
-//   },
-// ]);
