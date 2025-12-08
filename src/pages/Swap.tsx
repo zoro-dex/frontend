0f7e56a856efcd4b05495b396a095474110ba75c
Michael Mueller <mich@elmueller.net>
2025-12-08 16:29:32 +0100

    Upgrade deps

--- src/pages/Swap.tsx
+++ src/pages/Swap.tsx
@@ -1,6 +1,5 @@
 import { Footer } from '@/components/Footer';
 import { Header } from '@/components/Header';
-import { ModeToggle } from '@/components/ModeToggle';
 import { SwapSettings } from '@/components/SwapSettings';
 import { SwapSuccess } from '@/components/SwapSuccess';
 import { Button } from '@/components/ui/button';
